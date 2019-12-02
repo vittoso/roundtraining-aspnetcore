73783f2c848b8c4b2e33b9a2745e46e2abe5c723
Vittorio Sozzi <vittorio.sozzi@elfo.net>
2019-12-02 17:01:14 +0100

    Part 5

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,6 +1,8 @@
 ﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +13,36 @@ namespace CityInfo.API.Controllers
     [Route("/api/cities")]
     public class PointsOfInterestController : Controller
     {
+        private ILogger<PointsOfInterestController> _logger;
+        private IMailService _mailService;
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger,
+            IMailService mailService)
+        {
+            _logger = logger;
+            _mailService = mailService;
+        }
+
         [HttpGet("{cityId}/pointsofinterest")]
         public IActionResult GetPointsOfInterest(int cityId)
         {
-            var city = CitiesDataStore.Current.Citites.FirstOrDefault(c => c.Id == cityId);
+            try
+            {
+                var city = CitiesDataStore.Current.Citites.FirstOrDefault(c => c.Id == cityId);
 
-            if (city == null)
-                return NotFound();
+                if (city == null)
+                {
+                    _logger.LogInformation($"City with id {cityId} wasn't found when accessing points of interest.");
+                    return NotFound();
+                }
 
-            return Ok(city.PointsOfInterest);
+                return Ok(city.PointsOfInterest);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Exception while getting points of interest for city with id {cityId}.", ex);
+                return StatusCode(500, "A problem happened while handling your request.");
+            }
+            
         }
 
         [HttpGet("{cityId}/pointsofinterest/{id}", Name = "GetPointOfInterest")]
@@ -161,6 +184,9 @@ namespace CityInfo.API.Controllers
                 return NotFound();
 
             city.PointsOfInterest.Remove(poi);
+
+            _mailService.Send("Point of interest deleted.",
+                $"Point of interest {poi.Name} with id {poi.Id} was deleted");
 
             return NoContent(); // or return OK(poi);
         }
---
+++ CityInfo.API/Services/CloudMailService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Services
+{
+    public class CloudMailService : IMailService
+    {
+        private static string _mailTo = Startup.Configuration["mailSettings:mailToAddress"];
+        private static string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
+
+        public void Send(string subject, string message)
+        {
+            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with CloudMailService.");
+            Debug.WriteLine($"Subject: {subject}");
+            Debug.WriteLine($"Message: {message}");
+        }
+    }
+}
